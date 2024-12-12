d05b09e3f1ebb86600294c72126efa9f880ea468
Niklas H <156240330+mymh13@users.noreply.github.com>
2024-12-12 10:25:21 +0100

    added fodler

---
+++ PowerFightersXmas/PowerFightersXmas/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace PowerFightersXmas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello Marcus");
+        }
+    }
+}
