3a79355a872aaa82243208584dada076c09ae2c9
Niklas H <156240330+mymh13@users.noreply.github.com>
2024-12-12 14:37:19 +0100

    feature: added empty methods that will handle the menu-input

--- PowerFightersXmas/PowerFightersXmas/UI/MainMenu.cs
+++ PowerFightersXmas/PowerFightersXmas/UI/MainMenu.cs
@@ -22,7 +22,6 @@ namespace PowerFightersXmas.UI
 
         internal static void EntryMenu()
         {
-            string? entryMenuInput = string.Empty;
             Console.Clear();
             GameDisplay.DisplayColourMessage("\n\tChoose an option:\n", ConsoleColor.Yellow);
             Console.WriteLine("\t1. Start a new game");
@@ -30,26 +29,17 @@ namespace PowerFightersXmas.UI
             Console.WriteLine("\t3. Help / Instructions on how to play the game");
             GameDisplay.DisplayColourMessage("\t4. Quit (Why would you ever want to do that..?)\n", ConsoleColor.Red);
 
-            entryMenuInput = Console.ReadLine();
+            var entryMenuInput = Console.ReadLine();
             switch (entryMenuInput)
             {
                 case "1":
-                    // TODO; Start a new game
-                    Console.WriteLine("To be implemented. Returning to the EntryMenu, press any key:");
-                    Console.ReadKey();
-                    EntryMenu();
+                    StartNewGame();
                     break;
                 case "2":
-                    // TODO; Load a game
-                    Console.WriteLine("To be implemented. Returning to the EntryMenu, press any key:");
-                    Console.ReadKey();
-                    EntryMenu();
+                    LoadGame();
                     break;
                 case "3":
-                    // TODO; Display instructions
-                    Console.WriteLine("To be implemented. Returning to the EntryMenu, press any key:");
-                    Console.ReadKey();
-                    EntryMenu();
+                    Instructions();
                     break;
                 case "4":
                     GameDisplay.DisplayColourMessage("\n\tGoodbye! Evil Mage Marcus will come and haunt you forever!", ConsoleColor.Red);
@@ -61,12 +51,31 @@ namespace PowerFightersXmas.UI
             }
         }
 
+        internal static void StartNewGame()
+        {
+            // TODO; Start a new game
+            Console.WriteLine("To be implemented. Returning to the EntryMenu, press any key:");
+            Console.ReadKey();
+            EntryMenu();
+        }
+
+        internal static void LoadGame()
+        {
+            // TODO; Load a game
+            Console.WriteLine("To be implemented. Returning to the EntryMenu, press any key:");
+            Console.ReadKey();
+            EntryMenu();
+        }
+
         internal static void Instructions()
         {
             // TODO; Basic instructions on how to play the game
             Console.WriteLine("Basic instructions will be here.");
+            Console.WriteLine("To be implemented. Returning to the EntryMenu, press any key:");
+            Console.ReadKey();
+            EntryMenu();
         }
         
-        // Then we also need to create menu handlers for navigation and interactions between- and in the various rooms and objects
+        // TODO; Then we also need to create menu handlers for navigation and interactions between- and in the various rooms and objects
     }
 }
