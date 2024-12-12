496f91932be7a28d24e852e20107d7948ef2e937
Niklas H <156240330+mymh13@users.noreply.github.com>
2024-12-12 13:38:40 +0100

    Feature: Updated MainMenu with the EntryPoint and EntryMenu methods

--- PowerFightersXmas/PowerFightersXmas/UI/MainMenu.cs
+++ PowerFightersXmas/PowerFightersXmas/UI/MainMenu.cs
@@ -8,10 +8,28 @@ namespace PowerFightersXmas.UI
 {
     internal class MainMenu
     {
-        public static void StartMenu()
+        internal static void EntryPoint()
         {
-            Console.WriteLine("Have fun!");
+            GameDisplay.PrintCenteredText(" ______________________________________________________", ConsoleColor.Green);
+            GameDisplay.PrintCenteredText(" |                                                    |", ConsoleColor.Green);
+            GameDisplay.PrintCenteredText(" | Welcome to the Power Fighters Christmas adventure! |", ConsoleColor.Green);
+            GameDisplay.PrintCenteredText(" |                                                    |", ConsoleColor.Green);
+            GameDisplay.PrintCenteredText(" |       Press any key to get on with the show..      |", ConsoleColor.Green);
+            GameDisplay.PrintCenteredText(" |____________________________________________________|", ConsoleColor.Green);
             Console.ReadKey();
+            EntryMenu();
+        }
+
+        internal static void EntryMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1. Start a new game");
+            Console.WriteLine("2. Load a game"); // TODO; This requires us to save game states in a file
+            Console.WriteLine("3. Help / Instructions on how to play the game");
+            Console.WriteLine("4. Quit (Why would you ever want to do that..? Evil Mage Marcus will come and haunt you forever!)");
+
+            // Then we need to create menu handlers for navigation and interactions between- and in the various rooms and objects
         }
     }
 }
